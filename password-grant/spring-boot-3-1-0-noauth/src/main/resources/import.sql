9f64652af7ece1d3ec9bc18fb97b6b30efe130b6
Xico Utrabo <xico.utrabo@gmail.com>
2026-04-16 17:45:47 -0300

    Spring security checklist, not tested

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,7 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', ' $2a$10$7dBlwmk.MpDEtBhXToXoS.WSWFCqYO0hUnGAY5WWDlG5005ECw7wC
-');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', ' $2a$10$7dBlwmk.MpDEtBhXToXoS.WSWFCqYO0hUnGAY5WWDlG5005ECw7wC
-');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
